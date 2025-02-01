9401ac56718295245bb9a799f413b7fa06af7f9d
unknown <gauresh019@gmail.com>
2025-02-01 17:42:03 +0530

    first commit

---
+++ amazon-clone-frontend/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import Address from "./Components/Address";
+import Checkout from "./Components/Checkout";
+
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+
+import SignUp from "./Components/SignUp";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import AddProduct from "./Components/AddProduct";
+
+
+const promise = loadStripe(
+  "pk_test_51KUDBXSE1AGsrDtwyXK8vcHYNkEOofJAP1vV1fRlpZNo93g4o80dZe4IvhAkBXo2ytDciCqqpynwQUXv7plCjezF00G9zyj4sc"
+);
+
+function App() {
+  return (
+    <Router>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/address" element={<Address />} />
+         
+
+          <Route path="/addproduct" element={<AddProduct />} />
+                  </Routes>
+      </Container>
+    </Router>
+  );
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export default App;
